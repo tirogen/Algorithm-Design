221f5391125f30d7c90868c35226430be2fb9a40
Unknown <the-px@hotmail.com>
2019-02-10 17:07:21 +0700

    longest subsequence

--- ex03e1_lcs/another_solution.cpp
+++ ex03e1_lcs/another_solution.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int mem[501][501];
+int mem[500][500];
 
 int lcs(string one, string two, int i1, int i2){
     for(int i=1; i<=i1; i++){
         for(int j=1; j<=i2; j++){
-            if(one[i]==two[j]) return mem[i][j] = 1+mem[i-1][j-1];
-            return mem[i][j] = max(mem[i-1][j], mem[i][j-1]);
+            if(one[i]==two[j]) mem[i][j] = 1+mem[i-1][j-1];
+            else mem[i][j] = max(mem[i-1][j], mem[i][j-1]);
         }
     }
+    return mem[one.length()][two.length()];
 }
 
 int main(){
