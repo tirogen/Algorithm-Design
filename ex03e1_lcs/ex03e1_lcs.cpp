932aa1afb9047d4ae08536cc7ba7be7c901f3cf1
Unknown <the-px@hotmail.com>
2019-02-10 18:21:58 +0700

    python code

--- ex03e1_lcs/ex03e1_lcs.cpp
+++ ex03e1_lcs/ex03e1_lcs.cpp
@@ -3,17 +3,25 @@ using namespace std;
 
 int mem[501][501];
 
-int lcs(string one, string two, int i1, int i2){
-    for(int i=1; i<=i1; i++){
-        for(int j=1; j<=i2; j++){
-            if(one[i]==two[j]) return mem[i][j] = 1+mem[i-1][j-1];
-            return mem[i][j] = max(mem[i-1][j], mem[i][j-1]);
-        }
+int lcs(string one, string two, int pone, int ptwo){
+    if(pone == one.length() || ptwo == two.length()) return 0;
+    if(mem[pone][ptwo]!=-1) return mem[pone][ptwo];
+    if(one[pone] == two[ptwo]){
+        return mem[pone][ptwo] = 1+lcs(one, two, pone+1, ptwo+1);
+    }else{
+        return mem[pone][ptwo] = max(lcs(one, two, pone+1, ptwo), lcs(one, two, pone, ptwo+1));
     }
 }
 
 int main(){
+    for(int i=0; i<501; i++){
+        for(int j=0; j<501; j++){
+            mem[i][j]=-1;
+        }
+    }
+
     string one, two;
     cin >> one >> two;
-    cout << lcs(one, two, one.length(), two.length());
+    cout << lcs(one, two, 0, 0);
 }
+
