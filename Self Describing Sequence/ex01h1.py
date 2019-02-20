72147ea4fb78f8d0164603be1d86d4f0ff7656e1
Unknown <the-px@hotmail.com>
2019-02-20 22:35:40 +0700

    Self Describing Sequence

--- Self Describing Sequence/ex01h1.py
+++ Self Describing Sequence/ex01h1.py
@@ -1,9 +1,15 @@
-def sds(i, num, target):
-    if i==target: return num
-    
-
+import bisect as bs
 
 n=int(input())
+x=[]
 for e in range(n):
-    t=int(input())
-    x=sds(1,1,t)
+    x.append(int(input()))
+L=[0,1,3]
+MAX = max(x)
+num = 3
+while L[-1]<MAX:
+    i = bs.bisect_left(L,num)
+    L.append(L[-1]+i)
+    num += 1
+for o in x:
+    print(bs.bisect_left(L, o))
