630dbb57279d38df90f6cd0dd274bdf8d8869770
Unknown <the-px@hotmail.com>
2019-02-22 14:45:46 +0700

    Square Map

--- Square Map/ex03h1_square.py
+++ Square Map/ex03h1_square.py
@@ -1,28 +1,14 @@
 import numpy as np
 
-def arsum(i,j,k):
-    if i+k>r or j+k>c: return 0
-    s = np.sum(A[i:i+k,j:j+k])
-    if s == k**2: return k
-    else:
-        x = arsum(i+1,j,k)
-        if x != 0: return k
-        y = arsum(i,j+1,k)
-        if y != 0: return k
-        z = arsum(i+1,j+1,k)
-        return k if z !=0 else 0
-
 r,c = [int(e) for e in input().strip().split()]
-A = []
-for i in range(r):
-    S = []
-    for j in input().strip():
-        S.append(int(j))
-    A.append(S)
-A = np.array(A)
+A = np.zeros((r+1,c+1))
 
-for e in range(min(r,c),0,-1):
-    k = arsum(0,0,e)
-    if k!=0:
-        print(k)
-        exit()
+for i in range(1,r+1):
+    j = 1
+    for n in input().strip():
+        if n == "1":
+            A[i][j] = 1+min(A[i][j-1],A[i-1][j],A[i-1][j-1])
+        else:
+            A[i][j] = 0
+        j += 1
+print(int(np.max(A)))
