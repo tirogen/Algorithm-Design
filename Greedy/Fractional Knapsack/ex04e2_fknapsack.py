a0ad725b20448b6388947edd2e5e0169fe0fe0dd
Unknown <the-px@hotmail.com>
2019-03-20 00:51:11 +0700

    fixed fk napsack

--- Greedy/Fractional Knapsack/ex04e2_fknapsack.py
+++ Greedy/Fractional Knapsack/ex04e2_fknapsack.py
@@ -1,8 +1,7 @@
-import numpy as np
-W,n = map(np.float64, input().split())
-v = [np.float64(e) for e in input().split()]
-w = [np.float64(e) for e in input().split()]
-P = sorted([(np.divide(v[i],w[i],dtype=np.float64),i) for i in range(int(n))])[::-1]
+W,n = map(float, input().split())
+v = [float(e) for e in input().split()]
+w = [float(e) for e in input().split()]
+P = sorted([(v[i]/w[i],i) if w[i]!=0 else (float('inf'),i) for i in range(int(n))])[::-1]
 M = 0
 for f,i in P:
     if W > w[i]:
