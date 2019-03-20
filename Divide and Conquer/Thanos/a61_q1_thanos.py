a43bb4449e1c721d288ea5dcb0c05ad6fcb31400
Unknown <the-px@hotmail.com>
2019-03-20 18:35:39 +0700

    wtf!! thanos

--- Divide and Conquer/Thanos/a61_q1_thanos.py
+++ Divide and Conquer/Thanos/a61_q1_thanos.py
@@ -1,16 +1,15 @@
 from sys import stdin as kb
 import bisect as bs
 
-def thanos(L, R):
-    global Av,p,k,A,B
-    numAv = bs.bisect_right(Av ,R) - bs.bisect_left(Av, L)
-    if L == R:
-        return B*numAv if numAv > 0 else A
+def thanos(L,R,A,B):
+    if L>R: return 0
     mid = (L+R)//2
-    destroyAll = B*numAv*(R-L+1) if numAv > 0 else A
-    destroyLR = thanos(L, mid) + thanos(mid+1,R)
+    numAv=bs.bisect_right(Av,R)-bs.bisect_left(Av,L)
+    destroyAll = B*numAv*(R-L+1) if numAv>0 else A
+    if L==R or numAv==0: return destroyAll
+    destroyLR = thanos(L,mid,A,B)+thanos(mid+1,R,A,B)
     return min(destroyAll,destroyLR)
 
 p,k,A,B = [int(e) for e in kb.readline().split()]
 Av = sorted([int(e) for e in kb.readline().split()])
-print(thanos(1,pow(2,p)))
+print(thanos(1,pow(2,p),A,B))
