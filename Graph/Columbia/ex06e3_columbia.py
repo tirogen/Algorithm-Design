23736fc863d10d67232af9aefdc70d83d29ee0a6
Unknown <the-px@hotmail.com>
2019-03-23 01:13:17 +0700

    new columbia

--- Graph/Columbia/ex06e3_columbia.py
+++ Graph/Columbia/ex06e3_columbia.py
@@ -16,7 +16,7 @@ while len(heap)>0:
     inM[u][v]=True
     for x,y in {(0,1),(1,0),(0,-1),(-1,0)}:
         if v+x<c and u+y<r and v+x>=0 and u+y>=0:
-            if d[u+y][v+x] > w+g[u+y][v+x]:
+            if not inM[u+y][v+x] and d[u+y][v+x] > w+g[u+y][v+x]:
                 d[u+y][v+x]=w+g[u+y][v+x]
                 heapq.heappush(heap, (d[u+y][v+x], u+y, v+x))
 
